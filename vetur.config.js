a698b431d7e3313e93af1b2d45da46c908d6727d
Simon Gaudek <Simon.Gaudek@team-con.de>
2021-01-05 18:35:58 +0100

    chore(app): Config vetur

---
+++ vetur.config.js
@@ -0,0 +1,9 @@
+// vetur.config.js
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+    // **optional** default: `[{ root: './' }]`
+    // support monorepos
+    projects: [
+        "./app", // shorthand for only root.
+    ],
+};
